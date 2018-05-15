82586676177b4e4e020725c2f6437b9b6fd0d068
Alfred Mendoza <askdrax@gmail.com>
2018-05-15 13:09:21 -0400

    Completed inital CalculatePowers(int ToPower) method, Binding to the ListBox with a DataTemplate for each element, and Stopwatch.ElapsedTime display.

--- HeckinOnePointOne/MainWindow.xaml.cs
+++ HeckinOnePointOne/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Globalization;
+using System.Diagnostics;
 
 namespace HeckinOnePointOne
 {
@@ -20,9 +24,38 @@ namespace HeckinOnePointOne
     /// </summary>
     public partial class MainWindow : Window
     {
+        private PowerCollection thePowerCollection = new PowerCollection();
+
+        private Stopwatch timer;
+
+        public string TimerText;
+
+        public PowerCollection ThePowerCollection
+        {
+            get { return thePowerCollection; }
+            set
+            {
+                thePowerCollection = value;
+                NotifyStaticPropertyChanged("ThePowerCollection");      
+            }
+        }
+
         public MainWindow()
         {
+            timer = new Stopwatch();
             InitializeComponent();
+        }
+
+        public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged;
+
+        private static void NotifyStaticPropertyChanged(string propertyName)
+        {
+            StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public static void InitEventHandler()
+        {
+            //StaticPropertyChanged += OnStaticPropertyChanged;
         }
 
         private void MaximizeButton_Click(object sender, RoutedEventArgs e)
@@ -45,6 +78,45 @@ namespace HeckinOnePointOne
         private void MinimizeButton_Click(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PowersListBox.ItemsSource = ThePowerCollection;
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            thePowerCollection.Clear();
+            CalculatedTimerText.Text = "Calculating 1.1^0 to 1.1^" + NumberOfPowersInputBox.Text + "...";
+
+            timer.Reset();
+            timer.Start();
+
+            thePowerCollection.CalculatePowers(Convert.ToInt32(NumberOfPowersInputBox.Text));
+
+            timer.Stop();
+
+            double elapsed = timer.Elapsed.TotalMilliseconds;
+            string elapsedString;
+
+            if (elapsed >= 1000)
+            {
+                elapsedString = (elapsed / 1000).ToString() + " Seconds.";
+            }
+            else
+            {
+                elapsedString = elapsed.ToString() + " Milliseconds.";
+            }
+
+            CalculatedTimerText.Text = ("Calculated  [ 1.1^0 to 1.1^" + thePowerCollection.Last().PowerOf.ToString() + " ]  in " + elapsedString);
+        }
+
+        private void NumberOfPowersInputBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            int decimalNumber;
+
+            e.Handled = !int.TryParse(e.Text, NumberStyles.Integer, CultureInfo.CurrentCulture, out decimalNumber);
         }
     }
 }
---
+++ HeckinOnePointOne/Powers.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HeckinOnePointOne
+{
+    public class PowerCollection : ObservableCollection<Power>
+    {
+        public PowerCollection()
+        {
+
+        }
+
+        public void CalculatePowers(int ToPower)
+        {
+            this.Add(Power.GetTheOne());
+            if (ToPower >= 0)
+            {
+                for (int i = 0; i < ToPower; i++)
+                {
+                    Power thisPower = new Power();
+                    thisPower = thisPower.CalculateFromPrevious(this[i]);
+                    this.Add(thisPower);
+                }
+            }
+        }
+
+    }
+
+    public class Power
+    {
+        private int totalDigitCount;
+        private int wholeDigitCount;
+        private int fractionDigitCount;
+        private int decimalPointPosition;
+        private int powerOf;
+
+        private String powerOfString;
+        private String wholeValueString;
+        private String fractionValueString;
+        private String valueString;
+
+        private List<int> reverseValue = new List<int>();
+
+        public int TotalDigitCount
+        {
+            get { return totalDigitCount; }
+        }
+
+        public int WholeDigitCount
+        {
+            get { return wholeDigitCount; }
+        }
+
+        public int FractionDigitCount
+        {
+            get { return fractionDigitCount; }
+        }
+
+        public int DecimalPointPosition
+        {
+            get { return decimalPointPosition; }
+        }
+
+        public int PowerOf
+        {
+            get { return powerOf; }
+        }
+
+        public String PowerOfString
+        {
+            get { return powerOfString; }
+        }
+
+        public String WholeValueString
+        {
+            get { return wholeValueString; }
+        }
+
+        public String FractionValueString
+        {
+            get { return fractionValueString; }
+        }
+
+        public String ValueString
+        {
+            get { return valueString; }
+        }
+ 
+        public List<int> ReverseValue
+        {
+            get { return reverseValue; }
+        }
+
+        public Power()
+        {
+
+        }
+
+        public Power CalculateFromPrevious(Power previousPower)
+        {
+            Power newPower = new Power();
+
+            bool carryOne = false;
+            int sum = 0;
+
+            newPower.reverseValue.Add(1);
+            previousPower.reverseValue.Add(0);
+
+            for (int i = 1; i <= previousPower.totalDigitCount; i++)
+            {      
+                if (carryOne == true)
+                {
+                    sum = previousPower.reverseValue[i - 1] + previousPower.reverseValue[i] + 1;
+                }
+                else
+                {
+                    sum = previousPower.reverseValue[i - 1] + previousPower.reverseValue[i];
+                }             
+
+                if (sum > 9)
+                {
+                    sum -= 10;              
+                    carryOne = true;
+                }
+                else
+                {
+                    carryOne = false;
+                }
+
+                newPower.reverseValue.Add(sum);
+            }
+
+            if (carryOne == true)
+            {
+                newPower.reverseValue.Add(1);
+                newPower.wholeDigitCount = previousPower.wholeDigitCount + 1;
+                newPower.decimalPointPosition = previousPower.decimalPointPosition + 1;
+            }
+            else
+            {
+                newPower.wholeDigitCount = previousPower.wholeDigitCount;
+                newPower.decimalPointPosition = previousPower.decimalPointPosition;
+            }
+
+            newPower.powerOf = previousPower.powerOf + 1;
+            newPower.totalDigitCount = newPower.ReverseValue.Count();
+            newPower.fractionDigitCount = newPower.reverseValue.Count() - newPower.wholeDigitCount;
+
+            newPower.powerOfString = ("1.1^" + newPower.powerOf.ToString() + " = ");
+
+            newPower._GetValueString();
+            newPower._GetWholeValueString();
+            newPower._GetFractionValueString();
+            
+            carryOne = false;
+
+            return newPower;
+        }
+
+        public static Power GetTheOne()
+        {
+            Power theOne = new Power();
+
+            theOne.powerOf = 0;
+            theOne.decimalPointPosition = 1;
+            theOne.totalDigitCount = 1;
+            theOne.wholeDigitCount = 1;
+            theOne.fractionDigitCount = 0;
+            theOne.reverseValue.Add(1);
+            theOne.powerOfString = "1.1^0 = ";
+            theOne._GetValueString();
+            theOne._GetWholeValueString();
+            theOne._GetFractionValueString();
+            
+
+            return theOne;        
+        }
+
+        private void _GetValueString()
+        {
+            StringBuilder sb = new StringBuilder(this.totalDigitCount + 1);
+
+            for (int i = this.reverseValue.Count() - 1; i >= 0; i--)
+            {
+                sb.Append(this.reverseValue[i]);
+            }
+
+            if (this.totalDigitCount > 1)
+            {
+                sb.Insert(this.decimalPointPosition, ".");
+            }
+            
+            valueString = sb.ToString();
+        }
+
+        private void _GetWholeValueString()
+        {
+            StringBuilder sb = new StringBuilder(this.wholeDigitCount + 1);
+
+            sb.Append(valueString.Substring(0, this.decimalPointPosition));
+
+            wholeValueString = sb.ToString();
+        }
+
+        private void _GetFractionValueString()
+        {
+            StringBuilder sb = new StringBuilder(this.fractionDigitCount + 1);
+
+            if (this.fractionDigitCount > 0)
+            {
+                sb.Append(valueString.Substring(this.decimalPointPosition + 1));
+            }
+            
+
+            fractionValueString = sb.ToString();
+        }
+    }
+}
